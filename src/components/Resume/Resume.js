0f9de5ac3e5f5f2b754823cc881b28ce3ebaaa2e
“Erin <erinkelley27@gmail.com>
2018-09-25 11:39:18 -0400

    updated resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,8 +5,8 @@ class Resume extends Component {
   render () {
     return (
       <div className='resume'>
-        <a href='https://drive.google.com/file/d/1FKV6KNLkjhXf2sw_rFWE1w2EMk9Cc-eB/view?usp=sharing' target='_blank'><img src={require('./erinkelley_resume.png')} alt='resume' className='resume-img' /></a>
-        <a href='https://drive.google.com/file/d/1FKV6KNLkjhXf2sw_rFWE1w2EMk9Cc-eB/view?usp=sharing' target='_blank' className='click-for-site'><p className='click-for-site-text' id='resume-print-link'>Click for Printable View</p></a>
+        <a href='https://drive.google.com/file/d/1Yhlx0YmF82zUQX5whfS7CXY9F0HinFf5/view?usp=sharing' target='_blank'><img src={require('./erinkelley_resume.png')} alt='resume' className='resume-img' /></a>
+        <a href='https://drive.google.com/file/d/1Yhlx0YmF82zUQX5whfS7CXY9F0HinFf5/view?usp=sharing' target='_blank' className='click-for-site'><p className='click-for-site-text' id='resume-print-link'>Click for Printable View</p></a>
       </div>
     )
   }
